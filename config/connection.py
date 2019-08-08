0f1987bf1cee727f6f57b9e797136fb633ebbed1
mariatrapicyna <trmasha2015@yandex.ru>
2019-08-08 16:06:38 +0300

    add json file

--- config/connection.py
+++ config/connection.py
@@ -1,9 +1,8 @@
 import pymysql.cursors
 import pyodbc
+import json
 
-connection = pyodbc.connect("Driver={Devart ODBC Driver for MySQL};"
-                            "Server=localhost;"
-                            "Database=easample;"
-                            "USER=root;"
-                            "PASSWORD=root;"
-                            "OPTION=3;")
+with open("connect.json", "r") as read_file:
+    data = json.load(read_file)
+str_connect = 'Driver={};Server={};Database={};USER={};PASSWORD={};OPTION={};'.format(data['Driver'],data['Server'], data['Database'], data['User'], data['Password'], data['Option'])
+connection = pyodbc.connect(str_connect)
